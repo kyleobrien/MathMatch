786deb9b17ae8bae7018728a097b21aef6eb3e9e
Kyle O'Brien <me@kyleobrien.net>
2014-08-19 01:01:48 -0400

    Cleaned up the app delegate.

--- MathMatch/MathMatch/MMXAppDelegate.h
+++ MathMatch/MathMatch/MMXAppDelegate.h
@@ -13,9 +13,9 @@
 @property (nonatomic, strong) UIWindow *window;
 @property (nonatomic, weak) MMXNavigationController *navController;
 
-@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
-@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (nonatomic, strong, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, strong, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, strong, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
 
 - (void)saveContext;
 - (NSURL *)applicationDocumentsDirectory;
